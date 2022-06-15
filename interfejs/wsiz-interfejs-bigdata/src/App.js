4ce2fbe772c9ec0a4b3aed67233cdd608a027254
Patryk Święcicki <patswiecicki@gmail.com>
2022-06-15 18:24:31 +0200

    Add app inputs and output

--- interfejs/wsiz-interfejs-bigdata/src/App.js
+++ interfejs/wsiz-interfejs-bigdata/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {data} from "./data";
+import {Input} from "./Components/Input";
+import {useState} from "react";
 
 function App() {
+  const [fieldsValues, setFieldsValues] = useState({});
+  const [avg, setAvg] = useState(0);
+
+  const setFieldValue = (fieldName, fieldValue) => {
+    setFieldsValues((prevState => {
+      const newState = {
+        ...prevState,
+      }
+      newState[fieldName] = fieldValue;
+      return newState;
+    }))
+    setAvg(getAverage());
+  }
+
+  const getAverage = () => {
+    let sum = 0;
+    let num = 0;
+    for (const key in fieldsValues) {
+      sum += fieldsValues[key];
+      num+=1;
+    }
+    return Math.round((0.14+sum/num) * 20000) / 100;
+  }
+
+  const getResponseText  = `Szansa na odejście klienta: ${avg}%`;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"><br /><br />
+      {
+        data.map(inputData => {
+          return <div key={inputData.field}><Input name={inputData.field} options={inputData.options} onValueChane={setFieldValue}/> <br /><br /></div>
+        })
+      }
+      <br/>
+      <div>{getResponseText}</div>
     </div>
   );
 }
